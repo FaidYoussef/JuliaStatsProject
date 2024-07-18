0189cadf6c4d2da35a242c2f215d5f4eeaa7dd32
Youssef FAID <youssef.faid@grenoble-inp.org>
2024-07-18 17:07:49 +0200

    booya

---
+++ notebooks/Estim_WienerARD1_functions_revised.jl
@@ -0,0 +1,325 @@
+using Plots
+import Plots: plot!, vline!
+using LinearAlgebra
+using Optim
+using Statistics
+using Distributions
+using Random
+
+mutable struct WienerProcess{T<:Number}
+    mu::T
+    sigma2::T
+    timestep::T
+    values::Vector{T}
+    steps::Int
+    times::Vector{T}
+
+    # Constructeur pour initialiser le processus sans valeurs simulées
+    function WienerProcess{T}(mu::T, sigma2::T, timestep::T, steps::Int) where T<:Number
+        new{T}(mu, sigma2, timestep, Vector{T}(undef, steps + 1), steps, collect(0:timestep:steps * timestep))
+    end
+end
+
+# Fonction pour simuler le processus de Wiener
+function simulate!(wp::WienerProcess)
+    sqrt_sigma2 = sqrt(wp.sigma2)
+    wp.values = [0.0]
+    normal_dist = Normal(0, 1)  # Définir une distribution normale standard
+    for t in 1:wp.steps
+        dt = wp.timestep
+        dW = sqrt_sigma2 * sqrt(dt) * rand(normal_dist)
+        new_value = wp.values[end] + wp.mu * dt + dW
+        push!(wp.values, new_value)
+    end
+end
+
+# Méthode pour tracer le processus de Wiener
+function plot!(process::WienerProcess)
+    # Tracer le processus de Wiener
+    plot!(process.times, process.values, xlabel="Time", ylabel="Wiener Process", label="Wiener Process", 
+    linestyle=:dash, markercolor=:blue)
+    
+    # Tracer la droite mu*t
+    plot!(process.times, process.mu .* process.times, label="mu*t", linestyle=:dash)
+end
+
+mutable struct WienerARD1
+    underlying_process::WienerProcess{Float64}
+    rho::Float64
+    maintenance_times::Vector{Float64}
+    k::Int
+    values::Vector{Vector{Float64}}
+    new_times::Vector{Vector{Float64}}
+
+    function WienerARD1(mu::Float64, sigma2::Float64, timestep::Float64, steps::Int, rho::Float64, maintenance_times::Vector{Float64})
+        underlying_process = WienerProcess{Float64}(mu, sigma2, timestep, steps)
+        simulate!(underlying_process)
+        
+        k = length(maintenance_times)
+        
+        # Initialize new_times array
+        new_times = Vector{Vector{Float64}}()
+        start_time = 0.0
+        
+        # Populate new_times with intervals up to each maintenance time
+        for i in 1:length(maintenance_times)
+            end_time = maintenance_times[i]
+            push!(new_times,collect(start_time:timestep:end_time))
+            start_time = end_time
+        end
+        
+        # Last segment from last maintenance time to the end
+        push!(new_times, collect(start_time:timestep:(steps * timestep)))
+        
+        new(underlying_process, rho, maintenance_times, k, Vector{Vector{Float64}}(), new_times)
+    end
+end
+
+function simulate!(process::WienerARD1)
+    
+    # Créer un vecteur Y initialisé avec les valeurs de X
+    Y = copy(process.underlying_process.values)
+    
+    # Modifier les valeurs de Y selon les règles spécifiées
+    for i in 1:length(process.maintenance_times) - 1
+        maintenance_time = process.maintenance_times[i]
+        next_maintenance_time = process.maintenance_times[i+1]
+        
+        # Indices des temps entre deux instants de maintenance
+        indices = findall(t -> t > maintenance_time && t <= next_maintenance_time, process.underlying_process.times)
+        
+        # Mettre à jour les valeurs de Y
+        Y[indices] .-= process.rho .* process.underlying_process.values[indices[1] - 1]  
+    end
+    
+    indices = findall(t -> t > process.maintenance_times[length(process.maintenance_times)], process.underlying_process.times)
+    Y[indices] .-= process.rho .* process.underlying_process.values[indices[1] - 1]
+
+    new_values = Float64[]
+
+    last_before_maintenance = findlast(t -> t <= process.maintenance_times[1], process.underlying_process.times)
+
+    if last_before_maintenance !== nothing
+        # Concatenate the part before last_before_maintenance into new_values
+        append!(new_values, Y[1:last_before_maintenance])
+    end
+
+    for i in 1:length(process.maintenance_times) -1
+        mtn_time = process.maintenance_times[i]
+        next_mtn_time = process.maintenance_times[i+1]
+        between_maintenances = findall(t -> t > mtn_time && t<=next_mtn_time, process.underlying_process.times)
+        if !isempty(between_maintenances)
+            # Concatenate the part before last_before_maintenance into new_values
+            append!(new_values, (1 - process.rho) * process.underlying_process.values[between_maintenances[1]-1])
+            append!(new_values, Y[between_maintenances])
+        end
+    end
+    
+    m = findfirst(t -> t == process.maintenance_times[end], process.underlying_process.times)
+    append!(new_values, (1 - process.rho) * process.underlying_process.values[m])
+    append!(new_values, Y[m+1:end])
+
+    t1 = 1
+    t2 = 0
+   for i in 1:process.k+1
+        t2 = length(process.new_times[i]) + t2
+        push!(process.values, new_values[t1:t2])
+        t1 = t2 + 1
+   end
+end
+
+# Méthode pour tracer le processus de Wiener avec les temps de maintenance
+function plot!(process::WienerARD1)
+    # Tracer le processus de Wiener
+    # plot(process.underlying_process.times, process.values, xlabel="Time", ylabel="WienerARD1 Process", label="WienerARD1 Process", legend=:topright)
+    plot(xlabel="Time", ylabel="WienerARD1 Process", label="WienerARD1 Process")
+    for i in 1:process.k+1
+        plot!(process.new_times[i], process.values[i], linestyle=:dash, markercolor=:red, color=:blue, legend=:false)
+    end
+    # Tracer la droite mu*t
+    plot!(process.underlying_process.times, process.underlying_process.mu .* process.underlying_process.times, label="mu*t", linestyle=:dash)
+
+    vline!(process.maintenance_times, label="Maintenance Time", color=:red)
+end
+
+
+function observations_sampling(ps::WienerARD1, nb)
+    observations = Vector{Vector{Float64}}()
+    times = Vector{Vector{Float64}}()
+    indices = collect(1:nb:length(ps.values[1])-nb)
+    push!(observations, ps.values[1][indices])
+    push!(times, ps.new_times[1][indices])
+    for i in 2:ps.k + 1
+        # Generate a deterministic set of indices, e.g., every third element
+        indices = collect(nb+1:nb:length(ps.values[i])-nb)
+        push!(observations, ps.values[i][indices])
+        push!(times, ps.new_times[i][indices])
+    end
+    return observations, times
+end
+
+
+function s(j::Int, rho::Float64, k::Int, delta_t::Vector{Vector{Float64}})
+    if !(1 <= j <= k)
+        exit()
+    end
+    
+    if (j+1) > 2
+        return delta_t[j + 1][1] + rho * rho * delta_t[j][1] + (1 - rho) * (1 - rho) * delta_t[j][end]
+    else 
+        return delta_t[j + 1][1] + (1 - rho) * (1 - rho) * delta_t[j][end]
+    end
+end
+
+# Initialize Sigma matrix
+function Sigma(rho::Float64, k::Int, delta_t::Vector{Vector{Float64}})
+    
+    S = zeros(k, k)
+
+    # Populate Sigma using array comprehensions
+    for i in 1:k
+        S[i, i] = s(i, rho, k, delta_t)
+    end
+
+    for i in 1:(k - 1)
+        S[i, i+1] = - rho * delta_t[i][1]
+        S[i+1, i] = - rho * delta_t[i][1]
+   
+    end
+
+    return S
+end
+
+function u_revised(j::Int, rho::Float64, k::Int, vect::Vector{Vector{Float64}})
+    if !(1 <= j <= k)
+        exit()
+    end
+
+    if (j+1) > 2
+        return vect[j + 1][1] - rho * vect[j][1] + (1 - rho) * vect[j][end]
+    else  
+        return vect[j + 1][1] + (1 - rho) * vect[j][end]
+    end
+end
+
+function v_revised(j::Int, rho::Float64, k::Int, vect::Vector{Vector{Float64}})
+    
+    # if j<=1 
+    #     diff_values=diff(values[j][1:(end-1)])
+
+    # else
+    # diff_values = diff(values[j][2:(end-1)])
+    # end
+        
+    result = rho * sum(diff(vect[j][1:end]))
+    return result
+end
+
+function delta2(k::Int, vect::Vector{Vector{Float64}})
+    differences = Vector{Vector{Float64}}()
+    #differences=diff(values[1][1:(end-1)]) #si au moins 2 obs entre maint
+    # Parcourir les données
+    for j in 1:(k + 1)
+        push!(differences, diff(vect[j][1:(end)]))
+    end
+
+    return differences
+end
+
+function delta2_obs(k::Int, observations::Vector{Vector{Float64}})
+    differences=Vector{Vector{Float64}}()
+    # Parcourir les données
+    for j in 1:(k + 1)
+        push!(differences, diff(observations[j][1:(end)])) 
+    end
+
+    return differences
+end
+
+# observations, times = observations_sampling(psARD1, 1)
+# delta2_obs(psARD1.k,times)
+
+function z_revised(k::Int, vect::Vector{Vector{Float64}})
+    # Initialiser un vecteur pour stocker les différences successives
+    differences = Float64[]
+
+    # Parcourir les données
+    for i in 1:k
+        push!(differences, vect[i + 1][1] - vect[i][end]) # Δ! hypothèses:au - 2 obs entre 2 maintenances successives
+    end
+
+    return differences
+end
+
+# println(z(psARD1.k, psARD1.values))
+
+function mu_chapeau_revised(rho::Float64, k::Int, observations::Vector{Vector{Float64}}, delta_t::Vector{Vector{Float64}}) 
+    # observations, times = observations_sampling(psARD1, 1)
+    # Calculer l'inverse de Sigma
+    Sigma_inv = inv(Sigma(rho, k, delta_t))
+    
+    V = [v_revised(j, rho, k, observations) for j in 1:size(Sigma_inv)[1]] #/
+
+    U = [u_revised(j, rho, k, delta_t) for j in 1:length(V)] #/
+    
+    Z = z_revised(k, observations)
+    
+    # Calculer le produit transpose(u) * Sigma_inv_v
+    a = dot(U, Sigma_inv * Z)
+
+    b = dot(U, Sigma_inv * V)
+    
+    c = sum(map(sum, delta2_obs(k, observations)))
+    
+    d = dot(U, Sigma_inv * U)
+
+    e = sum(map(sum, delta_t))
+
+    return (a + b + c) / (d + e)
+end
+
+
+function sigma2_chapeau_revised(rho::Float64, k::Int, observations::Vector{Vector{Float64}}, delta_t::Vector{Vector{Float64}})
+    # observations, times = observations_sampling(psARD1, 1)
+    # Calculer l'inverse de Sigma
+    Sigma_inv = inv(Sigma(rho, k, delta_t))
+    
+    V = [v_revised(j, rho, k, observations) for j in 1:size(Sigma_inv)[1]] #/
+    
+    U = [u_revised(j, rho, k, delta_t) for j in 1:length(V)] #/
+    
+    Z = z_revised(k, observations)
+
+    mu = mu_chapeau_revised(rho, k, observations, delta_t)
+
+    # Calculer le produit transpose(u) * Sigma_inv_v
+    a = Z - mu .* U + V
+
+    b = dot(a, Sigma_inv * a)
+    
+    diffs = delta2_obs(k, observations)
+
+    d = 0
+    for (dy, dt) in zip(diffs, delta_t)
+        for (dy_j, dt_j) in zip(dy, dt)
+            d += ((dy_j - mu * dt_j)^2) / dt_j
+        end
+    end
+
+    N = sum(length(observations[i]) for i in 1:k + 1)
+    
+    return (b + d) / N
+end
+
+function objectif_revised(rho::Float64, k::Int, observations::Vector{Vector{Float64}}, delta_t::Vector{Vector{Float64}}, N::Int)
+    return N * log(sigma2_chapeau_revised(rho, k, observations, delta_t)) / 2 + log(sqrt(det(Sigma(rho, k, delta_t)))) 
+end
+
+function estimateur_rho_chapeau_revised(k::Int, observations::Vector{Vector{Float64}}, delta_t::Vector{Vector{Float64}})
+    
+    N = sum(length(observations[i]) for i in 1:k + 1)
+
+    result = optimize(rho -> objectif_revised(rho, k, observations, delta_t, N), 0.0, 1.0)  # Minimise sur l'intervalle [0.0, 1.0]
+
+    return Optim.minimizer(result)
+end
---
+++ notebooks/edf.jl
@@ -0,0 +1,175 @@
+include("fonctions.jl")
+include("Estim_WienerARD1_functions_revised.jl")
+
+chemin_données = "/home/AD/faidy/JuliaStatsProject/data/real_data/"
+chemin_projet = "/home/AD/faidy/JuliaStatsProject/"
+
+# Transformer le contenu de la feuille de IND-COL-1.xls en une matrice
+data_matrix_ind_col1 = readxlsheet(string(chemin_données, "Données_EDF_240611/Colmatage/IND-COL-1.xls"), "IND-COL-1")
+
+# Transformer le contenu de la feuille de IND-COL-2.xls en une matrice
+data_matrix_ind_col2 = readxlsheet(string(chemin_données, "Données_EDF_240611/Colmatage/IND-COL-2.xls"), "IND-COL-2")
+
+# Les cases vides de la colonne VALEUR des fichiers IND-COL-1.xls et IND-COL-2.xls ont été remplis par un objet DataValue{Union{}} 
+# qu'on le remplace par l'objet missing qui représente une donnée manquante en Julia
+isNA(x) = typeof(x) == DataValue{Union{}}
+valeurs = replace( x -> isNA(x) ? missing : x, data_matrix_ind_col1)
+
+data_matrix_ind_col1 = replace( x -> isNA(x) ? missing : x, data_matrix_ind_col1)
+data_matrix_ind_col2 = replace( x -> isNA(x) ? missing : x, data_matrix_ind_col2)
+
+df1 = MatrixToDataFrame(data_matrix_ind_col1)
+df1 = dropmissing(df1, :VALEUR)
+
+df2 = MatrixToDataFrame(data_matrix_ind_col2)
+df2 = dropmissing(df2, :VALEUR)
+
+nettoyages = readxlsheet(string(chemin_données, "Données_EDF_240611/Nettoyages/NETTOYAGES.xls"), "NETTOYAGES")
+df_nettoyages = MatrixToDataFrame(nettoyages)
+
+df_Infos_gen = readxlsheet(string(chemin_données, "Données_EDF_240611/Informations générales 9-7/HEURES.xls"), "HEURES")
+df_Infos_gen = MatrixToDataFrame(df_Infos_gen)
+
+# Création des enfants
+U2 = NAryTreeNode(missing, "U2", Vector{NAryTreeNode}())
+U3 = NAryTreeNode(missing, "U3", Vector{NAryTreeNode}())
+U5 = NAryTreeNode(missing, "U5", Vector{NAryTreeNode}())
+U9 = NAryTreeNode(missing, "U9", Vector{NAryTreeNode}())
+U10 = NAryTreeNode(missing, "U10", Vector{NAryTreeNode}())
+U18 = NAryTreeNode(missing, "U18", Vector{NAryTreeNode}())
+U19 = NAryTreeNode(missing, "U19", Vector{NAryTreeNode}())
+U8 = NAryTreeNode(missing, "U8", Vector{NAryTreeNode}())
+U13 = NAryTreeNode(missing, "U13", Vector{NAryTreeNode}())
+
+# Création du parent avec les enfants
+P1 = NAryTreeNode(missing, "P1", Vector([U2, U3, U5, U9, U10, U18, U19, U8, U13]))
+# Mettre à jour les parents des enfants
+for child in P1.children
+    child.parent = P1
+end
+
+# Création des enfants
+U1 = NAryTreeNode(missing, "U1", Vector{NAryTreeNode}())
+U4 = NAryTreeNode(missing, "U4", Vector{NAryTreeNode}())
+U11 = NAryTreeNode(missing, "U11", Vector{NAryTreeNode}())
+U12 = NAryTreeNode(missing, "U12", Vector{NAryTreeNode}())
+U14 = NAryTreeNode(missing, "U14", Vector{NAryTreeNode}())
+U15 = NAryTreeNode(missing, "U15", Vector{NAryTreeNode}())
+U16 = NAryTreeNode(missing, "U16", Vector{NAryTreeNode}())
+U17 = NAryTreeNode(missing, "U17", Vector{NAryTreeNode}())
+
+# Création du parent avec les enfants
+P2 = NAryTreeNode(missing, "P2", Vector([U1, U4, U11, U12, U14, U15, U16, U17]))
+
+# Mettre à jour les parents des enfants
+for child in P2.children
+    child.parent = P2
+end
+
+# Création des enfants
+U6 = NAryTreeNode(missing, "U6", Vector{NAryTreeNode}())
+U7 = NAryTreeNode(missing, "U7", Vector{NAryTreeNode}())
+
+# Création du parent avec les enfants
+P3 = NAryTreeNode(missing, "P3", Vector([U6, U7]))
+
+# Mettre à jour les parents des enfants
+for child in P3.children
+    child.parent = P3
+end
+
+paliers = [P1, P2, P3]
+
+for palier in paliers
+    unites = palier.children
+    for unite in unites
+        sous_unites = Vector(unique(df_Infos_gen[df_Infos_gen.UNITE .== unite.value, :].SOUS_UNITE))
+        for sous_unite in sous_unites
+            push!(unite.children, NAryTreeNode(unite, sous_unite, Vector{NAryTreeNode}()))
+        end
+    end
+end
+
+for palier in paliers
+    unites = palier.children
+    for unite in unites
+        sous_unites = unite.children
+        for sous_unite in sous_unites
+            push!(sous_unite.children, NAryTreeNode(sous_unite, "avant_RGV", Vector{NAryTreeNode}()))
+            push!(sous_unite.children, NAryTreeNode(sous_unite, "apres_RGV", Vector{NAryTreeNode}()))
+        end
+    end
+end
+
+for palier in paliers
+    unites = palier.children
+    for unite in unites
+        sous_unites = unite.children
+        for sous_unite in sous_unites
+            data_sous_unite_avant = filter(row -> row.UNITE .== unite.value 
+                                    && row.SOUS_UNITE .== sous_unite.value
+                                    && row[10] .== "ORIGINE",
+                                 df_Infos_gen)
+            data_sous_unite_apres = filter(row -> row.UNITE .== unite.value 
+                                    && row.SOUS_UNITE .== sous_unite.value
+                                    && row[10] .== "Remplacement",
+                                    df_Infos_gen)
+            circuits_avant = unique(data_sous_unite_avant.CIRCUIT)
+            circuits_apres = unique(data_sous_unite_apres.CIRCUIT)
+            for etat in sous_unite.children
+                if etat.value == "avant_RGV"
+                    for circuit in circuits_avant
+                        push!(etat.children, NAryTreeNode(etat, circuit, Vector{NAryTreeNode}()))
+                    end
+                else
+                    for circuit in circuits_apres
+                        push!(etat.children, NAryTreeNode(etat, circuit, Vector{NAryTreeNode}()))
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+# Spécifiez le chemin complet de votre répertoire
+directory = string(chemin_données, "Données_EDF_240611/Encrassement 2-7/")
+
+# Utilisez readdir() pour obtenir les noms des fichiers et des sous-répertoires
+files = readdir(directory)
+
+# Filtrer les fichiers pour ne garder que ceux qui ont l'extension .xlsx
+xlsx_files = filter(file -> endswith(file, ".xlsx"), files)
+xlsx_files = [file for file in xlsx_files if file != "~\$PERFOS_U2S4_3,48-3,47-3,49.xlsx" && file != "~\$PERFOS_U19S2_2,45-2,43-2,44.xlsx"]
+
+GVs_dict = set_GVs_dict(paliers)
+
+
+obs = GVs_dict[2.08].IND_COL_1.VALEUR_CHAUD
+times = GVs_dict[2.08].IND_COL_1.HEURES_MAT
+insert!(obs, 1, 0.)
+insert!(times, 1, 0.)
+
+obs = obs[2:end]
+times = times[2:end]
+
+obs = [x isa Number ? Float64(x) : tryparse(Float64, x) for x in obs]
+times = [x isa Number ? Float64(x) : tryparse(Float64, x) for x in times]
+delta_t = delta2(1, [times[1:2], times[3:end]])
+k = 1
+obsVect = [obs[1:2], obs[3:end]]
+
+rho = estimateur_rho_chapeau_revised(k, obsVect, delta_t)
+                                    
+
+s2 = sigma2_chapeau_revised(rho, k, obsVect, delta_t)
+mu = mu_chapeau_revised(rho, k, obsVect, delta_t)
+GVs_dict[2.08].IND_COL_1.VALEUR_CHAUD
+delta_t
+
+scatter(times, obs)
+
+psARD1 = WienerARD1(mu, s2, 10e4, 2000, rho, [167231.0] )
+simulate!(psARD1)
+
+GVs_dict[2.08].maintenances
